9067c565792d99f568c370888844b03e4ea61cda
tainarapatricia <tainara1921@live.com>
2025-07-28 23:04:14 -0300

    Atualização 28.07

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+// Botão do Whatsapp
+
+function openWhatsApp() {
+    const phoneNumber = '5531994466868'; 
+    const message = 'Olá, vim através do seu Portifólio!';
+    const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+}
+
+
+//PDF floricultura
+function irParaFloricultura(){
+    window.open("file:///C:/Users/taina/OneDrive/Documentos/Portfolio-main/docs/Floricultura-Online.pdf")
+
+}
+
+//PDF sekib
+function irParaSEKIB(){
+    window.open("file:///C:/Users/taina/OneDrive/Documentos/Portfolio-main/docs/SEKIB.pdf")
+
+}
+
+// Site Dr. Rozana
+
+function irParaDrRozana(){
+  window.open("https://landing-page-dr-rozana.vercel.app/#sobre")
+}
+
+// Aplicativo Gooday
+
+function irParaGooday(){
+  window.open("https://www.figma.com/design/0LldkORNZ9BE2HPnRIxSXA/Curso-Pro-Figma?node-id=75-35&t=P0azcSLCfTnPkBhm-1")
+}
+
+
+//Carrocel 
+
